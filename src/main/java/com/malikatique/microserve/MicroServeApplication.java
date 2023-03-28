9eab8f207959dfa972c92f569dce49f88bbdf0d3
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-03-28 11:52:20 +0500

    init

---
+++ src/main/java/com/malikatique/microserve/MicroServeApplication.java
@@ -0,0 +1,13 @@
+package com.malikatique.microserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malikatique/microserve/MicroServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malikatique.microserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

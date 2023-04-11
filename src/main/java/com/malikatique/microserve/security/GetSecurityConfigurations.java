872dc3290afc469d50b2d7146b0fa677c73f77d2
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-11 11:52:03 +0500

    inherit config

--- src/main/java/com/malikatique/microserve/security/GetSecurityConfigurations.java
+++ src/main/java/com/malikatique/microserve/security/GetSecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.malikatique.microserve.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,18 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@EnableWebSecurity
+@Component
 @RequiredArgsConstructor
-public class SecurityConfigurations {
+public class GetSecurityConfigurations {
 
     private final JwtAuthFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     private final MicoServiceSecurityConfig micoServiceSecurityConfig;
 
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("Package: filterChain");
         http

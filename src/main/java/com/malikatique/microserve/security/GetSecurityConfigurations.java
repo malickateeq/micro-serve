972f663f6f041c498f9423d5b333a972f1cf857a
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-11 12:08:49 +0500

    test without web

--- src/main/java/com/malikatique/microserve/security/GetSecurityConfigurations.java
+++ src/main/java/com/malikatique/microserve/security/GetSecurityConfigurations.java
@@ -1,9 +1,7 @@
 package com.malikatique.microserve.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,9 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class GetSecurityConfigurations {
 
@@ -22,6 +20,7 @@ public class GetSecurityConfigurations {
 
     private final MicoServiceSecurityConfig micoServiceSecurityConfig;
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("Package: filterChain");
         http

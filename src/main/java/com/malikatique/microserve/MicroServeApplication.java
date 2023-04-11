1b8621ee0e910d0269e9fdf562ee0ee3f95d1f74
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-11 13:19:59 +0500

    disable web component

--- src/main/java/com/malikatique/microserve/MicroServeApplication.java
+++ src/main/java/com/malikatique/microserve/MicroServeApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@Component
 public class MicroServeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServeApplication.class, args);

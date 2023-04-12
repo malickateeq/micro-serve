569b1567ac05e773ca334e01eb027337877889c4
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-12 14:25:27 +0500

    test-33

--- src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
+++ src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
@@ -1,25 +1,31 @@
 package com.malikatique.microserve.config;
 
 import com.malikatique.microserve.security.JwtService;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass(JwtService.class)
+@NoArgsConstructor
+@ConditionalOnClass({JwtService.class, TestService.class})
 public class CommonAutoConfiguration {
 
-    private final JwtService jwtService;
-
-    public CommonAutoConfiguration(JwtService jwtService) {
-        this.jwtService = jwtService;
-    }
+    @Autowired
+    private JwtService jwtService;
 
     @Bean
     @ConditionalOnMissingBean
     public JwtService jwtService() {
         return this.jwtService;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TestService testService() {
+        return new TestService();
     }
 
 }
---
+++ src/main/java/com/malikatique/microserve/config/TestService.java
@@ -0,0 +1,7 @@
+package com.malikatique.microserve.config;
+
+public class TestService {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}

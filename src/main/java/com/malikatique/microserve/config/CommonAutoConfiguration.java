6c3c5e92f2ac27022f15ff948f5556e4699d8972
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-12 14:15:32 +0500

    test-32

--- src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
+++ src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.malikatique.microserve.config;
 
-import com.malikatique.microserve.MicroServeApplication;
 import com.malikatique.microserve.security.JwtService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -8,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass(MicroServeApplication.class)
+@ConditionalOnClass(JwtService.class)
 public class CommonAutoConfiguration {
 
     private final JwtService jwtService;
@@ -16,16 +15,6 @@ public class CommonAutoConfiguration {
     public CommonAutoConfiguration(JwtService jwtService) {
         this.jwtService = jwtService;
     }
-
-//    @Autowired
-//    private SecurityConfigurations securityConfigurations;
-//
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public SecurityConfigurations securityConfig() {
-//        return securityConfigurations;
-//    }
-
 
     @Bean
     @ConditionalOnMissingBean

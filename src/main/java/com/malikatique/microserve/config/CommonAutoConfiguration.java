a19266f5126668366f914901afb37e812b906b70
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-12 15:46:15 +0500

    test-34

--- src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
+++ src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
@@ -1,15 +1,19 @@
 package com.malikatique.microserve.config;
 
 import com.malikatique.microserve.security.JwtService;
+import com.malikatique.microserve.utils.ApiResponse;
+import com.malikatique.microserve.utils.ObjectsValidator;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @NoArgsConstructor
+// @ComponentScan("com.malikatique.microserve.models") // will pick up any classes annotated with @Component, @Service, @Repository, etc.
 @ConditionalOnClass({JwtService.class, TestService.class})
 public class CommonAutoConfiguration {
 
@@ -26,6 +30,18 @@ public class CommonAutoConfiguration {
     @ConditionalOnMissingBean
     public TestService testService() {
         return new TestService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ApiResponse apiResponse() {
+        return new ApiResponse();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ObjectsValidator objectsValidator() {
+        return new ObjectsValidator();
     }
 
 }
--- src/main/java/com/malikatique/microserve/security/JwtAuthFilter.java
+++ src/main/java/com/malikatique/microserve/security/JwtAuthFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;

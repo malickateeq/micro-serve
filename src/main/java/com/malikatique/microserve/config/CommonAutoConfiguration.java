5a7547f772d814f2e61d32b5bde3e61e7c97e0ea
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-13 10:55:16 +0500

    test-37

--- src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
+++ src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
@@ -1,47 +1,47 @@
-package com.malikatique.microserve.config;
-
-import com.malikatique.microserve.security.JwtService;
-import com.malikatique.microserve.utils.ApiResponse;
-import com.malikatique.microserve.utils.ObjectsValidator;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@NoArgsConstructor
-// @ComponentScan("com.malikatique.microserve.models") // will pick up any classes annotated with @Component, @Service, @Repository, etc.
-@ConditionalOnClass({JwtService.class, TestService.class})
-public class CommonAutoConfiguration {
-
-    @Autowired
-    private JwtService jwtService;
-
-    @Bean
-    @ConditionalOnMissingBean
-    public JwtService jwtService() {
-        return this.jwtService;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public TestService testService() {
-        return new TestService();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ApiResponse apiResponse() {
-        return new ApiResponse();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ObjectsValidator objectsValidator() {
-        return new ObjectsValidator();
-    }
-
-}
+//package com.malikatique.microserve.config;
+//
+//import com.malikatique.microserve.security.JwtService;
+//import com.malikatique.microserve.utils.ApiResponse;
+//import com.malikatique.microserve.utils.ObjectsValidator;
+//import lombok.NoArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@NoArgsConstructor
+//// @ComponentScan("com.malikatique.microserve.models") // will pick up any classes annotated with @Component, @Service, @Repository, etc.
+//@ConditionalOnClass({JwtService.class, TestService.class})
+//public class CommonAutoConfiguration {
+//
+//    @Autowired
+//    private JwtService jwtService;
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public JwtService jwtService() {
+//        return this.jwtService;
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public TestService testService() {
+//        return new TestService();
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public ApiResponse apiResponse() {
+//        return new ApiResponse();
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public ObjectsValidator objectsValidator() {
+//        return new ObjectsValidator();
+//    }
+//
+//}
--- src/main/java/com/malikatique/microserve/config/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.malikatique.microserve.config;
-
-public class TestService {
-    public void hello() {
-        System.out.println("Hello world");
-    }
-}

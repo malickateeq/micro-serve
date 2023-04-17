629925b239c40eb0f2cd740f9ddf79a393ee4f95
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-17 17:09:53 +0500

    test-52

--- src/main/java/com/malikatique/microserve/config/MicroServeLoad.java
+++ src/main/java/com/malikatique/microserve/config/MicroServeLoad.java
@@ -1,11 +1,16 @@
 package com.malikatique.microserve.config;
 
-import com.malikatique.microserve.security.ApplicationConfig;
-import com.malikatique.microserve.security.SecurityConfigurations;
+import com.malikatique.microserve.security.*;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories(basePackages = "com.malikatique.microserve.repository")
-@Import({ ApplicationConfig.class, SecurityConfigurations.class})
+@Import({ 
+        ApplicationConfig.class, 
+        SecurityConfigurations.class, 
+        JwtAuthFilter.class, 
+        JwtService.class, 
+        MicoServiceSecurityConfig.class
+})
 public class MicroServeLoad {
 }

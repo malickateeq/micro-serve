3f8b2c9e25ec4f263c3ba27c2d3e5ad691cbf6b2
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2023-04-17 16:43:37 +0500

    test-49

--- src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
+++ src/main/java/com/malikatique/microserve/config/CommonAutoConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @NoArgsConstructor
 @Import({ ApplicationConfig.class, SecurityConfigurations.class})
-@ConditionalOnClass({JwtService.class, ApplicationConfig.class})
+//@ConditionalOnClass({JwtService.class, ApplicationConfig.class})
 public class CommonAutoConfiguration {
 
     @Autowired
